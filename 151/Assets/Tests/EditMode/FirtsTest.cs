474fb0b9d99fbff814f4e7fe47af5108a6277660
Ozker24 <49520232+Ozker24@users.noreply.github.com>
2021-01-25 00:01:18 +0100

    Initial proyect and Test

---
+++ 151/Assets/Tests/EditMode/FirtsTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class FirtsTest
+    {
+        [Test]
+        public void TheFirstTest()
+        {
+            Assert.AreEqual(2 + 2, 4);
+        }
+    }
+}
